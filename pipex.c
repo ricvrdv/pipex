a20d14ee614afe54548484874714291ac47a7f90
Ricardo Garcia <costagarcia.r95@gmail.com>
2025-02-05 14:24:29 +0000

    Working version

--- pipex.c
+++ pipex.c
@@ -35,38 +35,93 @@ static pid_t	fork_process()
 	return (pid);
 }
 
-static void	execute_command(char *cmd, char **envp)
+static void	ft_free(char *str[])
 {
-	char **arg_split;
+	int	i;
 
-	arg_split = ft_split(cmd, ' ');
-	if (!arg_split)
+	i = 0;
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
+
+static char	*find_dir(char *cmd, char **envp)
+{
+	char	*half_path;
+	char	**full_path;
+	char	*path;
+	int		i;
+
+	i = 0;
+	while (ft_strnstr(envp[i], "PATH=", 5) == 0)
+		i++;
+	full_path = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (full_path[i])
+	{
+		half_path = ft_strjoin(full_path[i], "/");
+		path = ft_strjoin(half_path, cmd);
+		free(half_path);
+		if (access(path, F_OK | X_OK) == 0)
+		{
+			ft_free(full_path);
+			return (path);
+		}
+		free(path);
+		i++;
+	}
+	ft_free(full_path);
+	return (NULL);
+}
+
+static void	execute_command(char *cmd, char **envp, int pipefd[])
+{
+	char	**cmd_split;
+	char	*path;
+
+	cmd_split = ft_split(cmd, ' ');
+	if (!cmd_split)
 	{
 		perror("Splitting the command into arguments");
 		exit(EXIT_FAILURE);
 	}
-	execve(arg_split[0], arg_split, envp);
-	perror("execv failed");
-	free_all(arg_split, count_words(cmd, ' '));
-	exit(EXIT_FAILURE);
+	path = find_dir(cmd_split[0], envp);
+	if (!path)
+	{
+		ft_free(cmd_split);
+		perror("invalid path");
+		close(pipefd[0]);
+		close(pipefd[1]);
+		exit(EXIT_FAILURE);
+	}
+	if (execve(path, cmd_split, envp) == -1)
+	{
+		ft_free(cmd_split);
+		close(pipefd[0]);
+		close(pipefd[1]);
+		exit(EXIT_FAILURE);
+	}
 }
 
-static void	setup_child1(int infile, int pipefd[], char *cmd1, char **envp)
+static void	setup_child(int infile, int pipefd[], char *cmd1, char **envp)
 {
 	close(pipefd[0]);
 	dup2(infile, STDIN_FILENO);
 	dup2(pipefd[1], STDOUT_FILENO);
-	close(pipefd[1]);
-	execute_command(cmd1, envp);
+	//close(pipefd[1]);
+	execute_command(cmd1, envp, pipefd);
 }
 
-static void	setup_child2(int outfile, int pipefd[], char *cmd2, char **envp)
+static void	setup_parent(int outfile, int pipefd[], char *cmd2, char **envp)
 {
 	close(pipefd[1]);
 	dup2(pipefd[0], STDIN_FILENO);
 	dup2(outfile, STDOUT_FILENO);
-	close(pipefd[0]);
-	execute_command(cmd2, envp);
+	//close(pipefd[0]);
+	execute_command(cmd2, envp, pipefd);
 }
 
 int main (int argc, char **argv, char **envp)
@@ -74,8 +129,7 @@ int main (int argc, char **argv, char **envp)
 	int		infile;
 	int		outfile;
 	int		pipefd[2];
-	pid_t	pid1;
-	pid_t	pid2;
+	pid_t	pid;
 
 	if (argc != 5)
 	{
@@ -85,21 +139,15 @@ int main (int argc, char **argv, char **envp)
 	infile = open_file(argv[1], O_RDONLY, 0);
 	outfile = open_file(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	create_pipe(pipefd);
-	pid1 = fork_process();
-	if (pid1 == 0)
+	pid = fork_process();
+	if (pid == 0)
 	{
-		setup_child1(infile, pipefd, argv[2], envp);
+		setup_child(infile, pipefd, argv[2], envp);
 	}
-	pid2 = fork_process();
-	if (pid2 == 0)
-	{
-		setup_child2(outfile, pipefd, argv[3], envp);
-	}
-	close(pipefd[0]);
-	close(pipefd[1]);
+	setup_parent(outfile, pipefd, argv[3], envp);
+	//close(pipefd[0]);
+	//close(pipefd[1]);
 	close(infile);
 	close(outfile);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
 	return (0);
 }
